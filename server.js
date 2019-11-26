a96479af675c242c842354f4a062c4f4022e349a
Chinonso Eke <Chinonsoeke@gmail.com>
2019-11-26 20:47:32 +0100

    Mods

--- server.js
+++ server.js
@@ -7,12 +7,27 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.get('/api/timestamp/:date_string?', (req, res) => {
     const { date_string } = req.params
 
-    var date = date_string ? new Date(date_string.includes('-') ? date_string : parseInt(date_string)) : new Date()
+    var date
 
-    res.json({
-        unix: date == 'Invalid Date' ? null : date.getTime(),
-        utc: date == 'Invalid Date' ? "Invalid Date" : date.toUTCString()
-    })
+    if (date_string && /^\d{5,}$/.test(date_string)) {
+
+        date = new Date(parseInt(date_string))
+    }
+    else {
+        date = date_string ? new Date(date_string) : new Date()
+    }
+
+    if (date == 'Invalid Date') {
+        return res.json({
+            error: 'Invalid Date'
+        })
+    }
+    else {
+        return res.json({
+            unix: date == 'Invalid Date' ? null : date.getTime(),
+            utc: date == 'Invalid Date' ? "Invalid Date" : date.toUTCString()
+        })
+    }
 })
 
 var listener = app.listen(process.env.PORT || 8000, function () {
